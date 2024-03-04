879b89d7d4dfdb5ad0058650143f90c00cb1a0a8
Michael Mathu <mikemathu.dev@gmail.com>
2024-03-04 19:18:53 +0300

    Add connection string to appsetting.json file. Register the connection string in Program.cs file.

--- Cards.Web/Program.cs
+++ Cards.Web/Program.cs
@@ -1,6 +1,11 @@
+using Cards.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RepositoryDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
