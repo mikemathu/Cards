9a22d7ce9b5488769a56abdae7aa68d1bf9ff738
Michael Mathu <mikemathu.dev@gmail.com>
2024-03-04 23:00:21 +0300

    Add Migrations; Initial Database creation, Change column data types and Data Seeding migration

---
+++ Cards.Persistence/Migrations/20240304182316_Initial Database Creation.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabaseCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    StatusId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                    table.ForeignKey(
+                        name: "FK_AppUsers_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    DateOfCreation = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    StatusId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AppUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardId);
+                    table.ForeignKey(
+                        name: "FK_Cards_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cards_Status_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_Email",
+                table: "AppUsers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_RoleId",
+                table: "AppUsers",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_AppUserId",
+                table: "Cards",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_Name",
+                table: "Cards",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_StatusId",
+                table: "Cards",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Status_Name",
+                table: "Status",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ Cards.Persistence/Migrations/20240304193853_Data Seeding.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"), "Member" },
+                    { new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"), "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Status",
+                columns: new[] { "StatusId", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("d7d4c053-49b6-410c-bc78-2d54a9991870"), "Done" },
+                    { new Guid("d8d4c053-49b6-410c-bc78-2d54a9991870"), "In Progress" },
+                    { new Guid("d9d4c053-49b6-410c-bc78-2d54a9991870"), "Todo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "AppUserId", "Email", "Password", "RoleId" },
+                values: new object[,]
+                {
+                    { new Guid("021ca3c1-0deb-4afd-ae94-2159a8479811"), "sue@gmail.com", "sueP@ssword", new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3") },
+                    { new Guid("80abbca8-664d-4b20-b5de-024705497d4a"), "john@gmail.com", "johnP@ssword", new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870") },
+                    { new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"), "kev@gmail.com", "kevP@ssword", new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cards",
+                columns: new[] { "CardId", "AppUserId", "Color", "DateOfCreation", "Description", "Name", "StatusId" },
+                values: new object[,]
+                {
+                    { new Guid("c9d4c057-49b6-410c-bc78-2d54a9991870"), new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"), "#000000", new DateTime(2024, 1, 15, 20, 37, 19, 0, DateTimeKind.Utc), "Installation of system to the new client.", "System Installation", new Guid("d8d4c053-49b6-410c-bc78-2d54a9991870") },
+                    { new Guid("c9d4c059-49b6-410c-bc78-2d54a9991870"), new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"), "#000000", new DateTime(2024, 1, 20, 20, 37, 19, 0, DateTimeKind.Utc), "The system has bags to be fixed", "Fix bugs", new Guid("d9d4c053-49b6-410c-bc78-2d54a9991870") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "AppUserId",
+                keyValue: new Guid("021ca3c1-0deb-4afd-ae94-2159a8479811"));
+
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "AppUserId",
+                keyValue: new Guid("80abbca8-664d-4b20-b5de-024705497d4a"));
+
+            migrationBuilder.DeleteData(
+                table: "Cards",
+                keyColumn: "CardId",
+                keyValue: new Guid("c9d4c057-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Cards",
+                keyColumn: "CardId",
+                keyValue: new Guid("c9d4c059-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "StatusId",
+                keyValue: new Guid("d7d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "AppUserId",
+                keyValue: new Guid("86dba8c0-d178-41e7-938c-ed49778fb52a"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "StatusId",
+                keyValue: new Guid("d8d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "StatusId",
+                keyValue: new Guid("d9d4c053-49b6-410c-bc78-2d54a9991870"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"));
+        }
+    }
+}
