61735a7fed3ea1e3a222431973b1e951f5b7c965
Michael Mathu <mikemathu.dev@gmail.com>
2024-03-12 21:06:03 +0300

    Add Card.Web class library

--- Cards.Web/Extensions/ServiceExtensions.cs
+++ Cards.Web/Extensions/ServiceExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Cards.Persistence;
+﻿using Cards.Domain.Contracts;
+using Cards.Persistence;
+using Cards.Persistence.Repositories;
+using Cards.Services;
+using Cards.Services.Abstraction;
+using Cards.Services.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cards.Web.Extensions
@@ -8,6 +13,21 @@ namespace Cards.Web.Extensions
         public static void ConfigureNpgsqlContext(this IServiceCollection services, IConfiguration configuration) =>
             services.AddDbContextPool<RepositoryDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        public static void ConfigureAutoMapper(this IServiceCollection services) =>
+            services.AddAutoMapper(typeof(MappingProfile));
+
+        public static void ConfigureCardService(this IServiceCollection services) =>
+            services.AddScoped<ICardService, CardService>();
+
+        public static void ConfigureCardRepository(this IServiceCollection services) =>
+           services.AddScoped<ICardRepository, CardRepository>();
+
+        public static void ConfigureAppUserRepository(this IServiceCollection services) =>
+           services.AddScoped<IAppUserRepository, AppUserRepository>(); 
+
+        public static void ConfigureUnitOfWorkRepository(this IServiceCollection services) =>
+           services.AddScoped<IUnitOfWork, UnitOfWork>();
 
     }
 }
--- Cards.Web/Program.cs
+++ Cards.Web/Program.cs
@@ -1,13 +1,20 @@
-using Cards.Persistence;
 using Cards.Web.Extensions;
-using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.ConfigureNpgsqlContext(builder.Configuration);
+builder.Services.ConfigureAutoMapper();
+builder.Services.ConfigureCardService();
+builder.Services.ConfigureCardRepository();
+builder.Services.ConfigureAppUserRepository();
+builder.Services.ConfigureUnitOfWorkRepository();
 
-builder.Services.AddControllers();
+
+builder.Services.AddControllers()
+.AddApplicationPart(Assembly.Load(new AssemblyName("Cards.Presentation")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
